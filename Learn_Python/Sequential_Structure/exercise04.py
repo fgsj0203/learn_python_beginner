6894169bca98467508467f0a5f59a70ce863d2c9
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:11:55 -0300

    calculate average four notes

---
+++ Learn_Python/Sequential_Structure/exercise04.py
@@ -0,0 +1,8 @@
+note_one = float(input("note one: "))
+note_two = float(input("note two: "))
+note_three = float(input("note three: "))
+note_four = float(input("note four: "))
+average_notes = (note_one + note_two + note_three + note_four) / 4.0
+
+#Printing value of average
+print("The average of notes is: ", average_notes)
