6d68ac04e059fa2836c5ab77810b8c99de759de0
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:05:57 -0300

    print sum of two numbers

---
+++ Learn_Python/Sequential_Structure/exercise03.py
@@ -0,0 +1,7 @@
+#Variables section
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+sum_numbers = number_one + number_two
+
+#Printing sum of numbers
+print("The sum of numbers is: ",sum_numbers)
