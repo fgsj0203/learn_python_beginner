7a0fb6a5a0a4479ac83421ecfb4d8023f2a8cd84
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:31:01 -0300

    calculate salary brute in month

---
+++ Learn_Python/Sequential_Structure/exercise08.py
@@ -0,0 +1,5 @@
+hours_worked_month = int(input("what hours worked in month: "))
+value_work_hour = float(input("What value of your hour work: "))
+salary_brute = hours_worked_month * value_work_hour
+
+print("Your salary brute in month is: R$", salary_brute)
