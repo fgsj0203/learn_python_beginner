2a0143e8b5113bdfec956660fb90c544fb92f3a3
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:57:47 -0300

    weight ideal for man and women

---
+++ Learn_Python/Sequential_Structure/exercise12.py
@@ -0,0 +1,7 @@
+height_people = float(input("What your height: "))
+
+weight_man = (72.7 * height_people) - 58
+weight_women = (62.1 * height_people) - 44.7
+
+print("Weight ideal for man is: %.1f " % weight_man)
+print("Weight ideal for women is: %.1f " % weight_women)
