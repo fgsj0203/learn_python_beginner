61e22ed76b1ea7b0697ca08d7df23575653181f8
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 15:32:27 -0300

    bigger two numbers

---
+++ Learn_Python/Conditional_Structure/exercise01.py
@@ -0,0 +1,8 @@
+number_one = int(input("number one: "))
+number_two = int(input("number two: "))
+
+if number_one > number_two:
+    print("number one is bigger", number_one)
+else:
+    print("number two is bigger", number_two)
+
