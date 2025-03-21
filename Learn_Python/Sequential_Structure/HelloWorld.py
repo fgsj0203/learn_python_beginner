b56e264ac57eebcd21ff8b6c57c1767db38a97ca
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 09:57:52 -0300

    first commit code python

---
+++ Learn_Python/Sequential_Structure/HelloWorld.py
@@ -0,0 +1,12 @@
+#Print "Hello World"
+
+print("Hello World")
+
+#-------------- Start function ---------------
+#Printing Hello World with function
+def printing_helloworld():
+    print("printing Hello World with function")
+
+#Calling function
+printing_helloworld()
+#-------------- Final function ----------------
