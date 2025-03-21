f42272d1783081d7b5cfa99c6a084e34e3e32832
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 15:56:51 -0300

    identify letter if consonant or vowel

---
+++ Learn_Python/Conditional_Structure/exercise04.py
@@ -0,0 +1,12 @@
+letter = str(input("Enter with a letter: "))
+letter_lower = letter[0].lower()
+
+if (letter_lower == "a") or (letter_lower == "e") or (letter_lower == "i") or (letter_lower == "o") or (letter_lower == "u"):
+    print("Your letter is vowel")
+else:
+    print("Your letter is consonant")
+
+
+
+
+
