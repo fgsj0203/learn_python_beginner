c070fd09dc3d9ef6cbbef3479e3e07a457da1af2
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:40:47 -0300

    converter temperature

---
+++ Learn_Python/Sequential_Structure/exercise09.py
@@ -0,0 +1,4 @@
+fahrenheit = int(input("What temperature in fahrenheit: "))
+converter_temperature_celsius = 5 * ((fahrenheit - 32) / 9)
+
+print("The temperature in fahrenheit is: Cº", converter_temperature_celsius)
