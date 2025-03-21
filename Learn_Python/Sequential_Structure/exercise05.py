68d7155f66a32f6efb45cf9764fa2e430a721485
Francisco Gomes <fgsj.dev@outlook.com>
2025-03-21 10:15:20 -0300

    calculate value converted meters for centimeters

---
+++ Learn_Python/Sequential_Structure/exercise05.py
@@ -0,0 +1,4 @@
+meters = int(input("input your size in meters: "))
+converter_meters_centimeters = meters * 100
+
+print(meters , "meters converted in centimeters is: ", converter_meters_centimeters, "cm")
